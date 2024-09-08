f8a7642fa0c629ec4405922ad356fc8b5f2337a8
urvi1622005 <urvashidhawan1622005@gmail.com>
2024-09-08 12:23:55 +0530

    .

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 import express from "express";
-
+import {connectDB }from "./db/connectDB.js";
+import dotenv from "dotenv";
 
 
 const app=express();
-
+dotenv.config();
 app.get("/",(req,res)=>{
     res.send("hello world! ");
 })
 
 app.listen(3000,()=>{
+    connectDB();
     console.log("server is working");
 });
